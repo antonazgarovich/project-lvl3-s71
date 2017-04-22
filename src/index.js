679a9c43929f59198e8802cbf17245169a29bc34
anton.azgarovich <anton.azgarovich@gmail.com>
2017-04-22 23:49:13 +0300

    added todo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import downloadPage from './downloadPage';
 import convertPageToLocal from './convertPageToLocal';
 import putPage from './putPage';
 
+// FIXME: don't download page if file exists
 export default (url, output = '.') =>
   downloadPage(url)
     .then(convertPageToLocal)
